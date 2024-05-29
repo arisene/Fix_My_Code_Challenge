8e46f13e069540d4fca1db149ac6ae2138598002
arisene <karuretwaarsene744@gmail.com>
2024-05-29 20:56:35 +0200

    do it

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -14,13 +14,13 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2])
+size = parseInt(process.argv[2], 16)
 
 for (let i = 0 ; i < (size) ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
 	process.stdout.write("#");
 	process.stdout.write(" ");
     }
-process.stdout.write("\n");
+    process.stdout.write("\n");
 }
 
